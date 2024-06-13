047ce2dd2e74af5a4a901497982edebc2aa35551
00dylan00 <dylandalton2021@gmail.com>
2024-06-13 15:47:56 +0200

    updated nbs

---
+++ documentation/personal_config.py
@@ -0,0 +1,15 @@
+# documentation/config.py
+
+# SGE Configuration
+sge_config = {
+    "host": "your_sge_host",
+    "username": "your_username",
+    "password": "your_password"
+}
+
+# Slurm Configuration
+slurm_config = {
+    "host": "irblogin01.irbbarcelona.pcb.ub.es",
+    "username": "ddalton",
+    "password": "Teclado$$11"
+}
